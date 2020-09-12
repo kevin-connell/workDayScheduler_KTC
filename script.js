5d7d54de132df315d1a88103bd9e0b42a7b989ca
FileOtter <kevin.t.connell@gmail.com>
2020-09-12 09:58:39 -0400

    small syntax error

--- script.js
+++ script.js
@@ -7,29 +7,29 @@ $("#currentDate").text(moment().format('dddd, MMMM Do YYYY'))
 for (hour = 9; hour < 18; hour++) {
     if (hour < moment().format('H')) {
         $("#" + hour).addClass("past");
-    } else {
-        if (hour = moment().format('H')) {
-            $("#" + hour).addClass("present");
-        } else {
-            $("#" + hour).addClass("future");
-        }
+    }
+    if (hour == moment().format('H')) {
+         $("#" + hour).addClass("present");
     } 
+    if (hour > moment().format('H')) {
+        $("#" + hour).addClass("future");
+    }
 }
 
 // retrieve local storage OR return emtpy array
 
-var schedule = JSON.parse(localStorage.getItem("localSchedule")) || ["" , "" , "" , "" , "" , "" , "" , "" , ""]
+var schedule = JSON.parse(localStorage.getItem("localSchedule")) || ["", "", "", "", "", "", "", "", ""]
 
 // insert the retrieved schedule data into the appropriate time blocks
 
 for (index = 0; index < schedule.length; index++) {
-    $("#" + (index + 9)).val(schedule[index]) 
+    $("#" + (index + 9)).val(schedule[index])
 }
 
 // if any save button is clicked, save that data to local storage
 
 $('button').click(function () {
     var indNum = (parseInt($(this).attr('id'))) - 9
-    schedule[indNum]= $("#" + (parseInt($(this).attr('id')))).val()
-    localStorage.setItem("localSchedule" , JSON.stringify(schedule))
+    schedule[indNum] = $("#" + (parseInt($(this).attr('id')))).val()
+    localStorage.setItem("localSchedule", JSON.stringify(schedule))
 });
